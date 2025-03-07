77911e0ccd5542358f3de1c97114060ec8aed864
ZTugberk <uslucant77@gmail.com>
2025-03-07 12:09:34 +0300

    Add files via upload

--- week3/src/FindGrade.java
+++ week3/src/FindGrade.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class FindGrade{
 
     public static void main(String[] args){
@@ -12,8 +14,20 @@ public class FindGrade{
         // 0 - 59 -> F
 
 
+        if(args.length>1) {
+int grade=Integer.parseInt(args[0]);
 
+            if (grade >= 90) {
+                System.out.println("Your grade is A");
+            } else if (grade >= 80 && grade < 90) {
+                System.out.println("Your grade is B");
+            } else if (grade >= 70 && grade < 80) {
+                System.out.println("Your grade is C");
+            } else if (grade >= 60 && grade < 70) {
+                System.out.println("Your grade is D");
+            } else System.out.println("Your grade is F");
 
+        }
 
     }
 
